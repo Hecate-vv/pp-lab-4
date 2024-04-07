5d24ceb034d383f502ddc8609d8ce41ebdb1e4a6
Hecate-vv <wdobrodziej@gmail.com>
2024-04-07 18:32:19 +0200

    zad2

--- Company.java
+++ Company.java
@@ -1,15 +1,17 @@
+import employees.Employee;
+import employees.Worker;
+import employees.Manager;
+
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Jakub Trytytka", 2500);
-        employees[1] = new Employee("Anna Maj", 2970);
+        employees[0] = new Manager("Jakub Trytytka", 2500, 0);
+        employees[1] = new Worker("Anna Maj", 2970, "Tester");
         employees[2] = new Employee("Jakub Musiał", 1000);
         employees[3] = new Employee("Bark Peppermint", 3000);
-        employees[4] = new Employee("Natasza Tulipan", 2900);
+        employees[4] = new Worker("Natasza Tulipan", 2900, "Sekretarka");
 
-        System.out.println("Pracownika o indeksie 3:");
-        System.out.println(employees[3]);
-
+        System.out.println("Dane dla wszystkich pracowników:");
         for (Employee emp : employees) {
             System.out.println(emp.toString());
         }
