13d0a5493974f3bda8d82be3b20dd131e1406bd9
Hecate-vv <wdobrodziej@gmail.com>
2024-04-07 19:05:36 +0200

    zad4

--- Company.java
+++ Company.java
@@ -4,24 +4,25 @@ import employees.Manager;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
         employees[0] = new Manager("Jakub Trytytka", 2500, 0);
         employees[1] = new Worker("Anna Maj", 2970, "Tester");
         employees[2] = new Employee("Jakub Musiał", 1000);
         employees[3] = new Employee("Bark Peppermint", 3000);
         employees[4] = new Worker("Natasza Tulipan", 2900, "Sekretarka");
+        employees[5] = new Worker("Bartolomeo Monaco", 3000, "MobileDeveloper");
+        employees[6] = new Manager("Liste Ring-Luck", 4000, 3);
 
-        int nonManagerEmployeeCount = 0;
-        for (Employee emp : employees) {
-            if (!(emp instanceof Manager)) {
-                nonManagerEmployeeCount++;
+        for (Employee employee : employees) {
+            employee.setSalary(employee.getSalary() + 500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(3);
+                employee.setSalary(75000);
             }
         }
-        ((Manager)employees[0]).setNumberOfSubordinates(nonManagerEmployeeCount);
-        employees[0].setSalary(7500);
-        System.out.println("Dane dla wszystkich pracowników:");
-        for (Employee emp : employees) {
-            System.out.println(emp.toString());
+        System.out.println("Zaktualizowane dane pracowników:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
         }
-    }
+      }
 }
